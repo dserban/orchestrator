238cb72db28639609b471c19ef85d37f91d2a40b
Dan Serban <dserban01@yahoo.com>
2018-07-04 19:07:08 +0200

    Update airflowbootstrap.sh

--- bootstrap/runasairflow/airflowbootstrap.sh
+++ bootstrap/runasairflow/airflowbootstrap.sh
@@ -1,1 +1,1 @@
-wget -qO /opt/Anaconda.sh https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+wget -qO /opt/tmp/Anaconda.sh https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
